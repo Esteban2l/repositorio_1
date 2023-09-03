0457c0cdc23fb91c1241d94f34d860c41aa25316
Pablo Lituma <estebanlituma2002@gmail.com>
2023-09-03 18:36:39 -0500

    Agregar Ventana Hola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hola Joaquín");
+        JOptionPane.showMessageDialog(null, "Holaa");
     }
 }
